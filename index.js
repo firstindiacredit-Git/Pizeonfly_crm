fa948a3113903e0baf00bac8f9024ca8b5a25eb5
amit <firsindiacredit786@gmail.com>
2024-09-26 16:31:55 +0530

    cors-error

--- index.js
+++ index.js
@@ -18,7 +18,22 @@ const path = require("path");
 dotenv.config();
 
 //Middleware setup
-app.use(cors());
+const allowedOrigins = ['https://crm.pizeonfly.com', 'http://localhost:5173'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,POST,PUT,DELETE,PATCH', 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+// Apply the CORS middleware with options
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.static("./uploads"));
 
