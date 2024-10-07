f483059a45ac496315f2174083a26dda28be8e5b
amit <firsindiacredit786@gmail.com>
2024-10-07 10:34:48 +0530

    upload setup

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.static("./uploads"));
+// app.use(express.static("./uploads"));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 // app.use(express.static(path.join(__dirname, 'dist')));
 
