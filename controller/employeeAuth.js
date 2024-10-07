9fbd435eca62b07813a2ce166bcd1457cbd24f5e
amit <firsindiacredit786@gmail.com>
2024-10-07 11:33:42 +0530

    kk

--- controller/employeeAuth.js
+++ controller/employeeAuth.js
@@ -36,12 +36,13 @@ router.get('/totalEmployees', async (req, res) => {
 // });
 router.post('/employees', uploadEmployee.single("employeeImage"), async (req, res) => {
     try {
-        console.log(req.file.path);
+        // console.log(req.file.path);
         const path = req.file?.path;
         // console.log(path); 
         // let newPath = path?.replace('uploads\\', "");
-        let newPath = path?.replace(/uploads[\\/]/, ""); // Handles both Windows and Linux slashes
-        console.log("Modified Path:", newPath);  // Debug the modified path
+        let newPath = path;
+        // let newPath = path?.replace(/uploads[\\/]/, ""); // Handles both Windows and Linux slashes
+        // console.log("Modified Path:", newPath);  // Debug the modified path
         if (newPath === undefined || newPath === null) {
             newPath = "default.jpeg"
         }
