f42de779db5c231e5abb4c301e45df5c6566f2e5
amit04-07-1999 <ameykumar76@gmail.com>
2025-07-18 12:40:58 +0530

    ggg

--- utils/emailService.js
+++ utils/emailService.js
@@ -21,7 +21,7 @@ const sendEmail = async (subject, message, recipients) => {
 
     try {
         await transporter.sendMail(mailOptions);
-        console.log("Email sent successfully");
+        // console.log("Email sent successfully");
     } catch (error) {
         console.error("Error sending email: ", error);
     }
